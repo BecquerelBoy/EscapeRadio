5c229a385c5d98e1060899d5281c7366a63e6db3
Mayeul <mayeul.ferrand@brassart.fr>
2025-05-05 15:29:24 +0200

    first Repository

---
+++ radio/Script/main.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var slider: HSlider = $HSlider
+@onready var label: Label = $Label
+@onready var win: Sprite2D = $win
+
+var timer := 0.0
+var tracking := false
+
+func _ready():
+	update_label(slider.value)
+	slider.value_changed.connect(update_label)
+	win.visible = false
+
+func update_label(value: float) -> void:
+	label.text = "%.1f" % value
+
+func _process(delta: float) -> void:
+	if label.text == "81.3":
+		if not tracking:
+			tracking = true
+			timer = 0.0
+		else:
+			timer += delta
+			if timer >= 1.0:
+				win.visible = true
+	else:
+		tracking = false
+		timer = 0.0
