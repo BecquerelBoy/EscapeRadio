93bdd7828f085424742978ddb78b96c9156da0bf
Mayeul <mayeul.ferrand@brassart.fr>
2025-05-07 10:32:30 +0200

    main menu ok

--- radio/Script/main.gd
+++ radio/Script/main.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 @onready var slider: HSlider = $Radio/HSlider
+@onready var yes: TextureButton = $Bouttons/Yes
+@onready var no: TextureButton = $Bouttons/No
+
 
 func _ready():
+	yes.disabled = true
+	no.disabled = true
 	update_value(slider.value)
 	slider.value_changed.connect(update_value)
 
