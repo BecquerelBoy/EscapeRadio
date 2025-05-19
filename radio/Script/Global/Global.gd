87a9bd32854908f4931f6fe0147176a7e0da86bb
Mayeul <mayeul.ferrand@brassart.fr>
2025-05-19 19:41:52 +0200

    update test enigme code

--- radio/Script/Global/Global.gd
+++ radio/Script/Global/Global.gd
@@ -6,26 +6,67 @@ var texte_en_cours: bool = false
 var current_text: int = 0
 var is_pausing: bool = false
 var slider_modifiable = true
-
+var reponses_joueur: Array = []
 var dialogues := {
 	1: {
-		"text": "En gros luden c'est un mythique euhh passive mythique",
+		"text": "Alors j'ai un code a 4 chiffre je commence par quoi ?",
 		"Rep1": 2,
-		"Rep2": 3,
-		"Rep3": 4,
-		"Rep4": 5
+		"Rep2": 2,
+		"Rep3": 2,
+		"Rep4": 2,
+		"Rep5": 2,
+		"Rep6": 2,
+		"Rep7": 2,
+		"Rep8": 2,
+		"Rep9": 2
 	},
 	2: {
-		"text": "perso je prend shadowflame"
+		"text": "Ok et maintenant ?",
+		"Rep1": 3,
+		"Rep2": 3,
+		"Rep3": 3,
+		"Rep4": 3,
+		"Rep5": 3,
+		"Rep6": 3,
+		"Rep7": 3,
+		"Rep8": 3,
+		"Rep9": 3
 	},
 	3: {
-		"text": "Rooh aller c'est pas si mal surtout qu'avec shadowflame ca monte a 14, comme j'ai les bottes ca fait 14+16 ca fait 30"
+		"text": "C'est fait, maintenant quoi ?",
+		"Rep1": 4,
+		"Rep2": 4,
+		"Rep3": 4,
+		"Rep4": 4,
+		"Rep5": 4,
+		"Rep6": 4,
+		"Rep7": 4,
+		"Rep8": 4,
+		"Rep9": 4
 	},
 	4: {
-		"text": "ok mais t'as oublié les MR mec"
+		"text": "Tres bien et pour le dernier ?",
+		"Rep1": 5,
+		"Rep2": 5,
+		"Rep3": 5,
+		"Rep4": 5,
+		"Rep5": 5,
+		"Rep6": 5,
+		"Rep7": 5,
+		"Rep8": 5,
+		"Rep9": 5
 	},
 	5: {
-		"text": "tu forces là fréro"
+		"text": "C'est bon j'ai tout activé je vais voir si il se passe qelques chose",
+	},
+	6: {
+		"text": "Lessgo c'est bon :D",
+	},
+	7: {
+		"text": "ok mais t'as oublié les MR mec",
+	},
+	8: {
+		"text": "tu forces là fréro",
 	}
 }
 
@@ -39,6 +80,18 @@ func _process(_delta: float) -> void:
 
 func afficher_texte_par_numero(numero: int) -> void:
 	if dialogues.has(numero):
+		# Gère les choix précédents (ne rien faire au tout début)
+		if numero == 1:
+			reponses_joueur.clear()
+
+		# Après le texte 5, on vérifie la combinaison
+		if current_text == 5:
+			if reponses_joueur == [8, 4, 7, 6]:
+				numero = 6
+			else:
+				numero = 1
+			reponses_joueur.clear()  # Réinitialiser pour une nouvelle séquence
+
 		var textebox = get_node("/root/Main/Textebox")
 		if textebox == null:
 			push_error("Textebox introuvable à /root/Main/Textebox")
