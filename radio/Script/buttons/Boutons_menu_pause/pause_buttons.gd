ebb12f9063e34a78c23d2d5bc9deaf093c3c835a
Mayeul <mayeul.ferrand@brassart.fr>
2025-05-19 11:54:10 +0200

    update

--- radio/Script/buttons/Boutons_menu_pause/pause_buttons.gd
+++ radio/Script/buttons/Boutons_menu_pause/pause_buttons.gd
@@ -8,7 +8,7 @@ func _on_pause_pressed() -> void:
 	click.play()
 	
 	# Instanciation de la scène pause_menu
-	var pause_scene_path = "res://pause_menu.tscn"
+	var pause_scene_path = "res://Scene/pause_menu.tscn"
 	if ResourceLoader.exists(pause_scene_path):
 		var pause_scene = load(pause_scene_path).instantiate()
 		# Ajoute la scène au niveau racine (ou à un nœud parent approprié)
