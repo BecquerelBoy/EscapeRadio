5f97ffcd22bc4a609acc7953a1622ce87ab29ae2
Mayeul <mayeul.ferrand@brassart.fr>
2025-06-03 16:59:35 +0200

    credit ok + menu pause fix

--- radio/Script/menu/credit.gd
+++ radio/Script/menu/credit.gd
@@ -1,8 +1,4 @@
 extends Control
 
-const MAIN_MENU = preload("res://Scene/main_menu.tscn")
-
-func _on_button_pressed() -> void:
-	TransitionScreen.transition()
-	await TransitionScreen.on_transition_finished
-	get_tree().change_scene_to_packed(MAIN_MENU)
+func _ready() -> void:
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
